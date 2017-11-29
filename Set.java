e523c7d8655c32ebbbb923325268c09b161ae0a0
Collin Hurley <collin@hurleyhome.com>
2017-11-28 20:46:35 -0500

    cleanup

--- Set.java
+++ Set.java
@@ -11,145 +11,120 @@
 */
 public class Set
 {
-   private int [] setArray = {};
-   public Set()
-   {
-     int [] setArray = new int[] {};//this works but is explicitly against
-                                    //his rules and makes me sad
-   }
-   public Set(int [] inputArray)
-   {
-      this.setArray = inputArray;
-   }
-	public void makeEmpty()
-   {
-      setArray = new int[]{};
-   }
-   public boolean isEmpty()
-   {
-    if (setArray.length > 0)
-      return false;
-    else 
-      return true;
-   }
-   public void add(int value)
-   {
-      if (!elementOf(value))
-      {
-    
-         int [] a = new int [setArray.length + 1];
-         for (int index = 0; index < setArray.length; index++)
-         {
-            a[index]= setArray[index];
-         }
-         setArray = a;
-      }
-   }
-   public int[] add(int value, int[] input)
-   {
-      if (!elementOf(value))
-      {
-    
-         int [] a = new int [input.length + 1];
-         for (int index = 0; index < input.length; index++)
-         {
-            a[index]= input[index];
-         }
-          return a;
-      }
-      else
-         return input;
-   }
+    private int[] setArray = {};
+    public Set()
+    {
+        setArray = new int[0];        //this works but is explicitly against his rules and makes me sad
+    }
 
-   public void remove(int value)
-   {
-      
-   }
-   public boolean elementOf(int number)
-   {
-      
-      for (int i =0; i <= setArray.length; i++)
-      {
-         if (setArray[i] == number)
+    public Set(int[] inputArray)
+    {
+        this.setArray = inputArray;
+    }
+
+    public Set(Set s) {
+        setArray = s.setArray.clone();
+    }
+
+    public void makeEmpty()
+    {
+        setArray = new int[0];
+    }
+
+    public boolean isEmpty()
+    {
+        if (setArray.length > 0)
+            return false;
+        else
             return true;
-      }
-         return false;
-   
-   }
-   public int size()
-   {
-      return setArray.length;
-   }
-   public Set union(Set input)
-   {
-      int [] tempArray = new int []{};
-      for (int index = 0; index <= input.setArray.length; index++)
-      {
-         add(setArray[index],tempArray);
-         add(input.setArray[index],tempArray);
-      }
-      Set output = new Set(tempArray);
-      return output;
-   }
-   public Set intersection(Set input)
-   {
-      int [] tempArray = new int []{};
-      for (int index = 0; index <= input.setArray.length; index++)
-      {
-         if(input.elementOf(setArray[index]))
-            add(setArray[index],tempArray);
-      }
-      Set output = new Set(tempArray);
-      return output;
-   }
-   public Set setDifference(Set input)
-   {
-      int [] tempArray = new int []{};
-      for (int index = 0; index <= input.setArray.length; index++)
-      {
-         boolean notSame = true;
-         for (int counter = 0; counter <= setArray.length; counter++)
-         {
-            if (input.setArray[index] == setArray[counter])
+    }
+
+    public void add(int value)
+    {
+        if (!elementOf(value))
+        {
+            int[] a = new int[setArray.length + 1];
+            for (int index = 0; index < setArray.length; index++)
             {
-               notSame = false;
+                a[index]= setArray[index];
             }
-            
-         }
-         if(notSame)
-         {
-             add(input.setArray[index],tempArray);
-         }
-      }
-      for (int index = 0; index <= input.setArray.length; index++)
-      {
-         boolean notSame = true;
-         for (int counter = 0; counter <= setArray.length; counter++)
-         {
-            if (setArray[index] == input.setArray[counter])
-            {
-               notSame = false;
+            a[setArray.length] = value;
+            setArray = a;
+        }
+    }
+
+    public void remove(int value)
+    {
+        if (elementOf(value)) {
+            int[] a = new int[setArray.length - 1];
+            int aindex = 0;
+            for (int index = 0; index < setArray.length; index++) {
+                if (setArray[index] != value) {
+                    a[aindex++] = value;
+                }
             }
-            
-         }
-         if(notSame)
-         {
-             add(setArray[index],tempArray);
-         }
-      }
-      Set output = new Set(tempArray);
-      return output;
-   }
-   public String toString()
-   {
-      String output;
-      output = "{ " + setArray[0];
-      for (int index = 1; index <= setArray.length; index++)
-      {
-         output += ", " + setArray[index];
-      }
-      output += " }";
-      return output;
-   }
-}  
+            setArray = a;
+        }
+    }
+
+    public boolean elementOf(int number)
+    {
+        for (int i =0; i <= setArray.length; i++)
+        {
+            if (setArray[i] == number)
+                return true;
+        }
+        return false;
+    }
+
+    public int size()
+    {
+        return setArray.length;
+    }
+
+    public Set union(Set input)
+    {
+        Set result = new Set(this);
+        for (int index = 0; index <= input.setArray.length; index++)
+        {
+            add(input.setArray[index]);
+        }
+        return result;
+    }
+
+    public Set intersection(Set input)
+    {
+        Set result = new Set();
+        for (int index = 0; index <= input.setArray.length; index++)
+        {
+            if(input.elementOf(setArray[index]))
+                result.add(setArray[index]);
+        }
+        return result;
+    }
+
+    public Set setDifference(Set input)
+    {
+        int [] tempArray = new int [] {};
+        Set result = new Set();
+        for (int index = 0; index <= input.setArray.length; index++)
+        {
+            if(!input.elementOf(setArray[index]))
+                result.add(setArray[index]);
+        }
+        return result;
+    }
+
+    public String toString()
+    {
+        String output;
+        output = "{ " + setArray[0];
+        for (int index = 1; index <= setArray.length; index++)
+        {
+            output += ", " + setArray[index];
+        }
+        output += " }";
+        return output;
+    }
+}
 
